7484be7630440ce6476ccbacea7d8720ef9c7bcd
Apollo910655 <crazo910@proton.me>
2025-02-02 17:19:33 -0800

    Delete Jojo_website directory

--- Jojo_website/static/date_choices.js
+++
@@ -1,4 +0,0 @@
-function selectDateOption(optionNumber) {
-    const date = new URLSearchParams(window.location.search).get('date');
-    window.location.href = `/confirmation?date=${date}&choice=${optionNumber}`;
-} 
